fbc9e9de737342cd1e69331d7bde375c54d60be9
Juan <juanquiross@gmail.com>
2022-04-05 10:23:40 -0300

    authenticated finalizado

--- api-rest-node/middlewares/authenticated.js
+++ api-rest-node/middlewares/authenticated.js
@@ -1,6 +1,37 @@
 'use strict'
+var jwt = require('jwt-simple');
+var moment = require('moment');
 
+var secret ='miclavesecretaparagenerareltoken-999';
 exports.authenticated = function(req, res, next){
-    console.log('middleware');
+    
+    // Comprobar si llega autorización
+    if(!req.headers.authorization){
+        return res.status(403).send({
+            message:'La peticion no tiene la cabecera de authorization'
+        });
+
+    }
+    //limpiar el token y quitar comillas
+    var token =req.headers.authorization.replace(/['"]+/g,'');
+    //decodificar token
+    try{
+        var payload = jwt.decode(token,secret);
+        //Comprobar si ha expirado
+        if(payload.exp <= moment().unix()){
+            return res.status(404).send({
+                message:'El token ha expirado'
+            });
+        }
+    }catch(ex){
+        return res.status(404).send({
+            message:'El token no es válido'
+        });
+    }
+    
+
+    //Adjuntar usuario identificado a la request
+    req.user = payload;
+    //Pasar a la accion
     next();
 }
